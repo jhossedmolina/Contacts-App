6001f8cdd7a74ed57782a56aa3cffa086adf6f49
Jhossed Pineda Molina <jhossedmolina@gmail.com>
2024-06-28 01:24:33 -0500

    Generating environment files, creating path aliases, installing Angular Material in the project, and configuring Firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    "projectId":"contactsdb-43e0e",
+    "appId":"1:481512990115:web:7f85b5c626545fffdba616",
+    "storageBucket":"contactsdb-43e0e.appspot.com",
+    "apiKey":"AIzaSyCwR1d3JzufGjQuWQK7G2w1z_TeX3AQCSQ",
+    "authDomain":"contactsdb-43e0e.firebaseapp.com",
+    "messagingSenderId":"481512990115"
+  }
+};
